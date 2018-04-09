1e006376b18bd19ca3d4f476fd5f61eda47f277b
andreasantarl <andrea.santarl@gmail.com>
2018-04-09 16:52:10 -0400

    Updates version of Elasticsearch to 5.1.2

--- scripts/elasticsearch.sh
+++ scripts/elasticsearch.sh
@@ -6,8 +6,8 @@ curl -s "http://localhost:9200" 2>&1 > /dev/null
 if [ $? != 0 ]; then
   yum install java-1.8.0-openjdk.x86_64 -y
   cd /tmp
-  wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.5.2.noarch.rpm
-  yum install elasticsearch-1.5.2.noarch.rpm -y
+  wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.1.2.rpm
+  yum install elasticsearch-5.1.2.noarch.rpm -y
   systemctl daemon-reload
   systemctl enable elasticsearch.service
   systemctl start elasticsearch.service
