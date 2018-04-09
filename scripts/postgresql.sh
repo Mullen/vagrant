9cefb153ccaf0cd5fc23046e59c11aa207cdea7c
andreasantarl <andrea.santarl@gmail.com>
2018-04-09 16:47:03 -0400

    Update postgresql.sh

--- scripts/postgresql.sh
+++ scripts/postgresql.sh
@@ -3,7 +3,7 @@
 # https://raw.githubusercontent.com/Mullen/vagrant/master/scripts/postgresql.sh
 
 hash psql 2>/dev/null || {
-  rpm -Uvh https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-centos10-10-1.noarch.rpm
+  rpm -Uvh https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-centos10-10-2.noarch.rpm
   yum install -y postgresql10-server postgresql10 postgresql10-libs postgresql10-contrib postgresql10-devel
   rm -rf /var/lib/pgsql/10/data
   /usr/pgsql-10/bin/postgresql-10-setup initdb
