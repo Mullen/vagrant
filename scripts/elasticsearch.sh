c160ab2a59ea929b7f62f980d8550db00b3f754f
andreasantarl <andrea.santarl@gmail.com>
2018-04-10 09:30:35 -0400

    Update elasticsearch.sh

--- scripts/elasticsearch.sh
+++ scripts/elasticsearch.sh
@@ -6,8 +6,11 @@ curl -s "http://localhost:9200" 2>&1 > /dev/null
 if [ $? != 0 ]; then
   yum install java-1.8.0-openjdk.x86_64 -y
   cd /tmp
-  wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.1.1.rpm
-  yum install elasticsearch-5.1.1.noarch.rpm -y
+#   wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.1.1.rpm
+#   yum install elasticsearch-5.1.1.noarch.rpm -y
+  wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.2.2.rpm
+  sha1sum elasticsearch-5.2.2.rpm 
+  sudo rpm --install elasticsearch-5.2.2.rpm
   systemctl daemon-reload
   systemctl enable elasticsearch.service
   systemctl start elasticsearch.service
