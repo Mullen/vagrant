1bae630cfbf8d23a828a1cee3166361fad9f7856
Dave Lee <sirdavidoflee@users.noreply.github.com>
2018-06-27 11:18:09 -0400

    Update php7.sh

--- scripts/php7.sh
+++ scripts/php7.sh
@@ -7,7 +7,7 @@ hash php 2>/dev/null || {
   rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm 
   yum install -y php71w php71w-opcache --skip-broken
   yum install -y yum-plugin-replace
-  yum replace -y php-common --replace-with=php71w-common
+  #yum replace -y php-common --replace-with=php71w-common
   yum install -y php71u-gd php71w-mysql php71w-mbstring php71w-intl
   cd /tmp
   curl -sS https://getcomposer.org/installer | php
