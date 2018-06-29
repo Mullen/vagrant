681df6033b5b85211aace6ff771a21de18942d13
Dave Lee <sirdavidoflee@users.noreply.github.com>
2018-06-29 12:16:03 -0400

    Updated for more PHP 7.2

--- scripts/php7.sh
+++ scripts/php7.sh
@@ -3,12 +3,14 @@
 # https://raw.githubusercontent.com/Mullen/vagrant/master/scripts/php7.sh
 
 hash php 2>/dev/null || {
-  rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm 
-  rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm 
-  yum install -y php70w php70w-opcache --skip-broken
-  yum install -y yum-plugin-replace
-  yum replace -y php-common --replace-with=php70w-common
-  yum install -y php70u-gd php70w-mysql php70w-mbstring php70w-intl
+  sudo yum install -y nano
+  sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+  sudo yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+  sudo yum install -y yum-utils
+  sudo yum-config-manager --enable remi-php72
+  sudo yum install -y php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo  php-mbstring php-intl php-xml php-dom
+  sudo yum -y install php-pecl-zendopcache
+  sed -i -e 's/AllowOverride None/AllowOverride All/g' /etc/httpd/conf/httpd.conf 
   cd /tmp
   curl -sS https://getcomposer.org/installer | php
   chmod +x composer.phar
