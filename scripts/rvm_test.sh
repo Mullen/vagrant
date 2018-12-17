986d0cc280e8b93df0562c0af6b2ff6d2c52aede
andreasantarl <andrea.santarl@gmail.com>
2018-12-17 12:24:29 -0500

    Adds in rvm install lines

--- scripts/rvm_test.sh
+++ scripts/rvm_test.sh
@@ -6,7 +6,7 @@ hash rvm 2>/dev/null || {
   command curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
   \curl -sSL https://get.rvm.io | bash -s stable
   source /home/vagrant/.rvm/scripts/rvm
-  # rvm install 2.5.3
-  # rvm use 2.5.3
+  rvm install 2.5.3
+  rvm use 2.5.3
   echo "gem: --no-rdoc --no-ri" >> ~/.gemrc
 } > /dev/null 2>&1
