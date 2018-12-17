c75c57bd1827e39a76e28a8fd6c0a29af7204318
andreasantarl <andrea.santarl@gmail.com>
2018-12-17 12:17:32 -0500

    More test

--- scripts/rvm_test.sh
+++ scripts/rvm_test.sh
@@ -3,9 +3,10 @@
 # https://raw.githubusercontent.com/Mullen/vagrant/master/scripts/rvm_te.sh
 
 hash rvm 2>/dev/null || {
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+  command curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
   \curl -sSL https://get.rvm.io | bash -s stable
-  rvm install 2.5.1
-  rvm use 2.5.1
+  source /home/vagrant/.rvm/scripts/rvm
+  # rvm install 2.5.3
+  # rvm use 2.5.3
   echo "gem: --no-rdoc --no-ri" >> ~/.gemrc
 } > /dev/null 2>&1
