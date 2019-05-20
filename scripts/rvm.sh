40e77d6bb72ae360214a717345b0956916cd0ea2
andreasantarl <andrea.santarl@gmail.com>
2019-05-20 10:22:02 -0400

    Update rvm.sh
    Updates to add GPG keys

--- scripts/rvm.sh
+++ scripts/rvm.sh
@@ -3,7 +3,7 @@
 # https://raw.githubusercontent.com/Mullen/vagrant/master/scripts/rvm.sh
 
 hash rvm 2>/dev/null || {
-  curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+  gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
   curl -sSL https://get.rvm.io | bash -s stable --gems=bundler
   echo "gem: --no-rdoc --no-ri" >> ~/.gemrc
 } > /dev/null 2>&1
