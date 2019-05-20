51c7f516d17d4fd794be32cb740be0b429e595e9
andreasantarl <andrea.santarl@gmail.com>
2019-05-20 10:39:02 -0400

    Update rvm.sh

--- scripts/rvm.sh
+++ scripts/rvm.sh
@@ -3,7 +3,7 @@
 # https://raw.githubusercontent.com/Mullen/vagrant/master/scripts/rvm.sh
 
 hash rvm 2>/dev/null || {
-  gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+  command curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
   curl -sSL https://get.rvm.io | bash -s stable --gems=bundler
   echo "gem: --no-rdoc --no-ri" >> ~/.gemrc
 } > /dev/null 2>&1
